4e604bcc316ebd440c238dc8e438b01d3bb09196
rajulsingh37 <rajulsingh37@gmail.com>
2019-07-03 23:17:26 +0530

    get state to redux store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import user from "./images/user.png";
 import "./App.css";
+import { createStore } from "redux";
+import reducer from "./reducers";
+
+const intialState = {
+  name: "Rajul Singh",
+  description: "Software Engineer SDE2",
+  likes: "Books and music",
+  location: "localhost"
+};
+
+const store = createStore(reducer, intialState);
 
 class App extends Component {
-  state = {
-    name: "Alex Bakery",
-    description: "Software Engineer, Speaker, and Occasional Model",
-    likes: "Cats, Wine, and Black dresses",
-    location: "localhost"
-  };
   render() {
-    const { name, description, likes, location } = this.state;
+    const state = store.getState();
+    const { name, description, likes, location } = state;
     return (
       <div className="App">
         <section className="User__img">
